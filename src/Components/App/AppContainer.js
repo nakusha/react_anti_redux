deec40ca2094139369223b4c2411cde77a6dccec
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-10-27 20:55:30 +0900

    5 Update Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,16 +3,25 @@ import AppPresenter from "./AppPresenter";
 import Store from "../../store";
 
 class AppContainer extends Component{
-    state = {
-        message: "Hello"
-    }
-
-    componentDidMount = () => {
-        setTimeout(() => {
-            this.setState({
-                message:"bye"
-            })
-        }, 2000);
+    constructor(props){
+        //provvider에 포함될 함수는 생성자에 있어야한다.
+        super(props);
+        //provider에서 사용할 함수 생성
+        this._changeMessage = () => {
+            if (this.state.message === "Hello"){
+                this.setState({
+                    message: "Bye bye"
+                });
+            } else{
+                this.setState({
+                    message:"Hello"
+                });
+            }
+        }
+        this.state = {
+            message: "Hello",
+            changeMessage: this._changeMessage
+        };
     }
     render(){
         return (
