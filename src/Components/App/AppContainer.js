70a32d31390778fb2b4ff1a3ec77896cf0334663
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-10-27 21:50:22 +0900

    7,  Counting Unseen ?Notifications

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -13,7 +13,7 @@ class AppContainer extends Component{
                 "1":{
                     id:1,
                     text: "Something",
-                    seen: false
+                    seen: true
                 },
                 "2":{
                     id:2,
