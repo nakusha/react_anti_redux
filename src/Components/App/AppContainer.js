432ceb7ee454b5c04c67eb41207d934c98d9d3f1
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-10-27 21:44:01 +0900

    6. rendering notification

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -7,20 +7,30 @@ class AppContainer extends Component{
         //provvider에 포함될 함수는 생성자에 있어야한다.
         super(props);
         //provider에서 사용할 함수 생성
-        this._changeMessage = () => {
-            if (this.state.message === "Hello"){
-                this.setState({
-                    message: "Bye bye"
-                });
-            } else{
-                this.setState({
-                    message:"Hello"
-                });
-            }
-        }
+        
         this.state = {
-            message: "Hello",
-            changeMessage: this._changeMessage
+            notifications:{
+                "1":{
+                    id:1,
+                    text: "Something",
+                    seen: false
+                },
+                "2":{
+                    id:2,
+                    text: "Something else",
+                    seen: false
+                },
+                "3":{
+                    id:3,
+                    text: "Something else but different",
+                    seen: false
+                },
+                "4":{
+                    id:4,
+                    text: "Something will happen",
+                    seen: false
+                }
+            }
         };
     }
     render(){
--- src/Components/App/AppPresenter.js
+++ src/Components/App/AppPresenter.js
@@ -5,6 +5,7 @@ import Notification from "../Notification";
 
 import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
+import Store from "../../store";
 
 const GlobalStyle = createGlobalStyle`
     ${reset};
@@ -17,7 +18,17 @@ const AppPresenter = () => (
         <GlobalStyle/>
         <Header/>
         <Flex alignCenter full column>
-            <Notification text={"Hello"} seen={false}/>
+            <Store.Consumer>
+                {store => {
+                    return Object.keys(store.notifications).map(key => (
+                        <Notification 
+                            key={store.notifications[key].id} 
+                            id={store.notifications[key].id} 
+                            text={store.notifications[key].text} 
+                            seen={store.notifications[key].seen} />
+                    ));
+                }}
+            </Store.Consumer>
         </Flex>
     </Fragment>
 );
